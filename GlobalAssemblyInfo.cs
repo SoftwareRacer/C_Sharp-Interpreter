1190bd2b4b461bbabd7a510543b422ec71a229c9
Marco Hennermann <79541316+SoftwareRacer@users.noreply.github.com>
2021-04-04 23:22:46 +0200

    Add files via upload

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Davide Icardi")]
+[assembly: AssemblyProduct("Dynamic Expresso")]
+[assembly: AssemblyDescription("C# expression interpreter/evaluator. See https://github.com/davideicardi/DynamicExpresso.")]
+[assembly: AssemblyCopyright("Copyright © Davide Icardi 2015")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.3.1.0")]
+
