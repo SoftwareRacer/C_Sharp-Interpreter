60dca4d9c77ccc752ddb6e9254cb67aa8e650d7a
Marco Hennermann <79541316+SoftwareRacer@users.noreply.github.com>
2021-04-22 11:14:33 +0200

    Update InterpreterOptions.cs

--- InterpreterOptions.cs
+++ InterpreterOptions.cs
@@ -1,34 +1,22 @@
 ﻿using System;
 
-namespace DynamicExpresso
+namespace Interpreter
 {
 	[Flags]
 	public enum InterpreterOptions
 	{
 		None = 0,
-		/// <summary>
-		/// Load primitive types like 'string', 'double', 'int', 'DateTime', 'Guid', ... See also LanguageConstants.CSharpPrimitiveTypes and LanguageConstants.PrimitiveTypes
-		/// </summary>
+		// Load primitive types like 'string', 'double', 'int', 'DateTime', 'Guid', ... See also LanguageConstants.CSharpPrimitiveTypes and LanguageConstants.PrimitiveTypes
 		PrimitiveTypes = 1,
-		/// <summary>
-		/// Load system keywords like 'true', 'false', 'null'. See also LanguageConstants.Literals.
-		/// </summary>
+		// Load system keywords like 'true', 'false', 'null'. See also LanguageConstants.Literals.
 		SystemKeywords = 2,
-		/// <summary>
-		/// Load common types like 'System.Math', 'System.Convert', 'System.Linq.Enumerable'. See also LanguageConstants.CommonTypes.
-		/// </summary>
+		// Load common types like 'System.Math', 'System.Convert', 'System.Linq.Enumerable'. See also LanguageConstants.CommonTypes.
 		CommonTypes = 4,
-		/// <summary>
-		/// Variables and parameters names are case insensitive.
-		/// </summary>
+		// Variables and parameters names are case insensitive.
 		CaseInsensitive = 8,
-		/// <summary>
-		/// Load all default configurations: PrimitiveTypes + SystemKeywords + CommonTypes
-		/// </summary>
+		// Load all default configurations: PrimitiveTypes + SystemKeywords + CommonTypes
 		Default = PrimitiveTypes | SystemKeywords | CommonTypes,
-		/// <summary>
-		/// Load all default configurations: PrimitiveTypes + SystemKeywords + CommonTypes + CaseInsensitive
-		/// </summary>
+		// Load all default configurations: PrimitiveTypes + SystemKeywords + CommonTypes + CaseInsensitive
 		DefaultCaseInsensitive = PrimitiveTypes | SystemKeywords | CommonTypes | CaseInsensitive,
 	}
 }
