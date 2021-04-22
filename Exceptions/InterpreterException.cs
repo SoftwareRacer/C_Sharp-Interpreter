351e9a0fac77dc7bb5b6c9a1fc3528fe6ea6448c
Marco Hennermann <79541316+SoftwareRacer@users.noreply.github.com>
2021-04-22 10:44:33 +0200

    Update and rename DynamicExpressoException.cs to InterpreterException.cs

---
+++ Exceptions/InterpreterException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interpreter.Exceptions
+{
+#if !NETSTANDARD1_6
+	[Serializable]
+#endif
+	public class InterpreterException : Exception
+	{
+		public InterpreterException() { }
+		public InterpreterException(string message) : base(message) { }
+		public InterpreterException(string message, Exception inner) : base(message, inner) { }
+
+#if !NETSTANDARD1_6
+		protected InterpreterException(
+		System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context)
+			: base(info, context) { }
+#endif
+	}
+}
